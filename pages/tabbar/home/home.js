5f9dc5c231de72274e1218379b1515c04389f398
wangkai <kwowkiller@163.com>
2018-12-06 10:01:23 +0800

    style

--- pages/tabbar/home/home.js
+++ pages/tabbar/home/home.js
@@ -4,7 +4,7 @@ Page({
   data: {
     // 组件所需的参数
     nvabarData: {
-      showCapsule: true, //是否显示左上角图标
+      showCapsule: false, //是否显示左上角图标
       title: '附件商家', //导航栏 中间的标题
       transparent: false //透明导航栏
     },
--- pages/tabbar/shoplist/shoplist.js
+++ pages/tabbar/shoplist/shoplist.js
@@ -6,7 +6,7 @@ Page({
    */
   data: {
     nvabarData: {
-      showCapsule: true, //是否显示左上角图标
+      showCapsule: false, //是否显示左上角图标
       title: '全部商家', //导航栏 中间的标题
       transparent: false //透明导航栏
     },
