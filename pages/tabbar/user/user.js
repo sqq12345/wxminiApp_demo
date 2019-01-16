a6746a66b68ba2f19728a860cb5e23c7f9a4a32c
huanglou <huanglou_168@163.com>
2019-01-16 15:19:53 +0800

    个人中心体现金额bug修改;底部导航图片修改;

--- pages/tabbar/user/user.js
+++ pages/tabbar/user/user.js
@@ -1,22 +1,15 @@
-// pages/tabbar/user/user.js
 import http from '../../../utils/http';
 import login from '../../../stores/Login';
-const { regeneratorRuntime } = global;
+
+const {regeneratorRuntime} = global;
 const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-/*
-    nvabarData: {
-      showCapsule: false, //是否显示左上角图标
-      title: '个人中心', //导航栏 中间的标题
-      transparent: true, //透明导航栏
-      color: '#000'
-    },
-*/
     userinfo: {
       points: 0,
       fansnum: 0,
@@ -28,27 +21,33 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: async function (options) {
-
+  onLoad: async function (options) {},
+  onShow: async function () {
+    this.getMycenter()
   },
-    onShow:async function(){
-      this.getMycenter()
-    },
-    getMycenter:async function () {
-        const result = await login();
-        http.request({
-            url: '/api/user/center',
-            header: {
-                token: result.user_token
-            },
-            method: 'POST',
-            success: (response) => {
-                const user = response.data.data;
-                user.earnings = Number.parseFloat(user.earnings).toFixed(2)
-                this.setData({
-                    userinfo: user
-                });
-            }
-        });
-    },
-})
+  getMycenter: async function () {
+    const result = await login();
+    http.request({
+      url: '/api/user/center',
+      header: {
+        token: result.user_token
+      },
+      method: 'POST',
+      success: (response) => {
+        const user = response.data.data;
+        if (user.earnings === null) {
+          this.setData({
+            userinfo: user,
+            earnings: Number(0),
+          });
+        }
+        else {
+          this.setData({
+            userinfo: user,
+            earnings: Number(user.earnings).toFixed(2),
+          });
+        }
+      }
+    });
+  },
+});
--- pages/user/draw/draw.js
+++ pages/user/draw/draw.js
@@ -1,24 +1,17 @@
-// pages/user/draw/draw.js
-const app = getApp();
 import http from '../../../utils/http';
 import login from '../../../stores/Login';
 import regex from '../../../utils/regex';
 import verify from '../../../utils/verify';
-const { regeneratorRuntime } = global;
+
+const app = getApp();
+const {regeneratorRuntime} = global;
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    /*
-        nvabarData: {
-          showCapsule: true, //是否显示左上角图标
-          title: '提现', //导航栏 中间的标题
-          transparent: true, //透明导航栏
-          color: '#fff'
-        },
-    */
     occupation: app.globalData.height + 46,
     form: {
       card: '',
@@ -31,7 +24,7 @@ Page({
 
   onInput(e) {
     const value = e.detail.value;
-    const { field } = e.target.dataset;
+    const {field} = e.target.dataset;
     this.setData({
       ['form.' + field]: value
     });
@@ -40,10 +33,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const max = Number.parseFloat(options.max).toFixed(2);
+  onLoad: function (o) {
+    console.log(o);
     this.setData({
-      max
+      max: o.max,
     });
   },
 
@@ -77,8 +70,6 @@ Page({
         }
       })
     }
-
-
   },
 
   /**
@@ -89,7 +80,7 @@ Page({
       'form.amount': this.data.max
     });
   }
-})
+});
 
 const config = {
   card: {
@@ -105,4 +96,4 @@ const config = {
     require: true,
     name: '收款银行',
   },
-}
+};
