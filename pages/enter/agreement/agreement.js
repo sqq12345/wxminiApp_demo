d2adcc5627ba5fe03bf0e94dfe98967db2ab1fec
liushuang <996398692@qq.com>
2019-03-06 17:00:44 +0800

    【细节处理】协议

--- pages/enter/agreement/agreement.js
+++ pages/enter/agreement/agreement.js
@@ -13,14 +13,32 @@ Page({
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: async function () {
+    onLoad: async function (option) {
+        const {vId} = option
+        console.log(option,vId)
+        if(vId==2){
+            this.getSolitaire()
+            return;
+        }
         this.getData()
     },
     onShow: async function () {
     },
-    getData: async function () {
+    //商家入驻协议
+    async getData () {
         http.request({
             url: '/api/shop/relief',
+            method: 'GET',
+            success: (res) => {
+                const detail = res.data.data;
+                this.setData({ detail })
+            }
+        });
+    },
+    //群接龙服务协议
+    async getSolitaire(){
+        http.request({
+            url: '/api/solitaire/relief',
             method: 'GET',
             success: (res) => {
                 const detail = res.data.data;
